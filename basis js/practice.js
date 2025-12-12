7c3f02776d03290ba35e0dc4dcd90044afb93af0
Shivam Dubey <shivamdubey55349@gmail.com>
2025-12-12 19:58:02 +0530

    practise done
    practise done

--- basis js/practice.js
+++ basis js/practice.js
@@ -1,10 +1,3 @@
-let month ="july";
-
-if(month==="january"){
-    console.log("winter is here");
-}
-else if(month==="aprail"){
-    console.log("summer is heree");
-}else{
-    console.log("error months");
-}
+let passward = prompt("set your password");
+let newPass =password.trim();
+ console.log(password);
