15f8c3160d7481f3bbfe310e661adc2211efc7ec
Shivam Dubey <shivamdubey55349@gmail.com>
2026-04-15 00:05:06 +0530

    learning  React
    learning  React

---
+++ first-react-app/src/App.jsx
@@ -0,0 +1,24 @@
+import KgButton from "./KgButton";
+import Hello from "./Hello";
+import Random from "./Random";
+
+function App()  {
+  return <div>
+   <h1> 
+     hello shivam
+   </h1>
+   <h2> this is a my react app</h2>
+    
+    <KgButton></KgButton>
+    <Hello/>
+    
+    <Random/>
+    <Random/>
+    <Random/>
+    <Random/>
+    <Random/>
+
+  </div>
+}
+
+export default App;
---
+++ first-react-app/src/Hello.jsx
@@ -0,0 +1,14 @@
+function Hello() {
+
+    let myName = 'shivam';
+    let number = 456;
+    let fullName = () => {
+        return 'shivam dubey';
+
+    }
+    return <h3>
+         messageNo : {number}Hello this is the future speaking. i am your maste {fullName()}
+    </h3>
+}
+
+export default Hello;
